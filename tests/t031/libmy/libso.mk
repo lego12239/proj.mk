ee09f0543acfe02689cee7ab00dc7bed73f82d95
Oleg Nemanov <lego12239@yandex.ru>
2021-07-16 20:36:42 +0300

    fix autotests

--- tests/t031/libmy/libso.mk
+++ tests/t031/libmy/libso.mk
@@ -2,5 +2,8 @@ PROJECT := libmy
 TARGET := libmy.so
 SOURCES := src1.c src2.c src3.c
 
-include libmy.proj.mk.info
+DEPS := libmya
+deps_get_libmya ?= cp $(PROJDIR)/libmya
+deps_patch_libmya := libmya.patch
+
 include .proj.mk/c-proj.mk
