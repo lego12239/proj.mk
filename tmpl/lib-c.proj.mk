56282863514012d510df0cb8ac6e302ac06ebd07
Oleg Nemanov <lego12239@yandex.ru>
2021-07-19 19:29:13 +0300

    add templates makefiles

---
+++ tmpl/lib-c.proj.mk
@@ -0,0 +1,13 @@
+PROJECT := libmy
+TARGET_TYPE ?= liba
+SOURCES := src1.c src2.c src3.c
+
+#DEPS := libmya
+#deps_get_libmya ?= ln $(PROJDIR)/libmya
+
+include .proj.mk/c-proj.mk
+
+install: $(TARGET)
+	install -d $(D)/lib $(D)/include
+	install -m 0444 $(TARGET) $(D)/lib
+	install -m 0444 my.h $(D)/include
---
+++ tmpl/prog-c.proj.mk
@@ -0,0 +1,14 @@
+PROJECT := prog
+SOURCES := main.c another.c
+
+DEPS := libmy
+deps_get_libmy := cp $(shell pwd)/libmy
+#deps_patch_libmy := libmy.patch libmy.fix.patch
+#deps_ttype_libmya := libso
+
+include .proj.mk/c-proj.mk
+
+install: $(TARGET)
+	install -d $(D)/bin
+	install -m 0444 $(TARGET) $(D)/bin
+
