23107cad2255238099f2b12cd26518c7e9e5df5a
Oleg Nemanov <lego12239@yandex.ru>
2021-07-19 20:24:26 +0300

    add makefile template for project local lib

---
+++ tmpl/projlocallib-c.proj.mk
@@ -0,0 +1,14 @@
+PROJECT := libmy
+TARGET_TYPE ?= liba
+SOURCES := src1.c src2.c src3.c
+
+#DEPS := libmya
+#deps_get_libmya ?= ln $(PROJDIR)/libmya
+
+PROJMKDIR := $(shell A=`pwd`; while [ ! -d $$A/.proj.mk ] ; do A=$${A%/*}; done; echo $$A/.proj.mk)
+include $(PROJMKDIR)/c-proj.mk
+
+install: $(TARGET)
+	install -d $(D)/lib $(D)/include
+	install -m 0444 $(TARGET) $(D)/lib
+	install -m 0444 my.h $(D)/include
